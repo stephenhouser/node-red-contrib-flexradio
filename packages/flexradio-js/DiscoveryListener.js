e550be24ae1bd2231639c1fe8b02e31f05bf998e
Stephen Houser <stephenhouser@gmail.com>
2021-09-29 21:46:34 -0400

    refactor discovery listener into singleton

--- packages/flexradio-js/DiscoveryListener.js
+++ packages/flexradio-js/DiscoveryListener.js
@@ -1,10 +1,47 @@
+/*
+{
+	"type": "discovery",
+	"stream": 2048,
+	"sequence": 1,
+	"payload": {
+		"discovery_protocol_version": "3.0.0.1",
+		"model": "FLEX-6600M",
+		"serial": "0621-1104-6601-1641",
+		"version": "3.2.39.3374",
+		"nickname": "Flex-6600M",
+		"callsign": "N1SH",
+		"ip": "192.168.10.27",
+		"port": 4992,
+		"status": "Available",
+		"inuse_ip": 0,
+		"inuse_host": 0,
+		"max_licensed_version": "v3",
+		"radio_license_id": "00-1C-2D-05-1A-68",
+		"requires_additional_license": 0,
+		"fpc_mac": "00:1C:2D:03:85:6A",
+		"wan_connected": 1,
+		"licensed_clients": 2,
+		"available_clients": 2,
+		"max_panadapters": 4,
+		"available_panadapters": 4,
+		"max_slices": 4,
+		"available_slices": 4,
+		"gui_client_ips": 0,
+		"gui_client_hosts": 0,
+		"gui_client_programs": 0,
+		"gui_client_stations": 0,
+		"gui_client_handles": 0
+	}
+}
+*/
 const udp = require('dgram');
 const EventEmitter = require('events');
-
 const flex = require('./flex');
 
 const log_info = function(msg) { console.log(msg); };
 const log_debug = function(msg) { };
+
+const FLEX_DISCOVERY_PORT = 4992;
 
 const DiscoveryStates = {
 	connecting: 'connecting',
@@ -18,7 +55,7 @@ class DiscoveryListener extends EventEmitter {
 		super();
 
 		this.host = host || '0.0.0.0';
-		this.port = port || 4992;
+		this.port = port || FLEX_DISCOVERY_PORT;
 		this.discoveryState = DiscoveryStates.stopped;
 		this.discoverySocket = null;
 
@@ -41,13 +78,13 @@ class DiscoveryListener extends EventEmitter {
 
 	get radios() {
 		// Return a copy not the original in case the caller modifies it
-		return { ...this.discovered_radios };
+		return this.discovered_radios;
 	}
 
 	_startDiscoveryListener() {
 		log_info('DiscoveryListener::_startDiscoveryListener()');
 
-		this.discoverySocket = udp.createSocket({type: 'udp4', reuseAddr: true});
+		this.discoverySocket = udp.createSocket({ type: 'udp4', reuseAddr: true });
 		const discoverySocket = this.discoverySocket;
 
 		const discoveryListener = this;
@@ -83,8 +120,11 @@ class DiscoveryListener extends EventEmitter {
 		// const discoveryListener = this;
 		const flex_msg = flex.decode_realtime(data);
 		if (flex_msg.type === 'discovery') {
-				log_debug('DiscoveryListener::_receiveData(' + JSON.stringify(flex_msg) + ')');
+			log_debug('DiscoveryListener::_receiveData(' + JSON.stringify(flex_msg) + ')');
+			if (flex_msg.payload && flex_msg.payload.serial) {
+				this.discovered_radios[flex_msg.payload.serial] = flex_msg.payload;
 				this.emit('discovery', flex_msg);
+			}
 		}
 	}
 
@@ -101,4 +141,17 @@ class DiscoveryListener extends EventEmitter {
 	}
 }
 
-module.exports = { DiscoveryListener: DiscoveryListener };
+// Singleton for discovery listener
+// As we cannot properly "share" the UDP port and may need this in
+// multiple places.
+let _discovery_listener = null;
+function discovery_listener() {
+	if (!_discovery_listener) {
+		_discovery_listener = new DiscoveryListener('0.0.0.0', FLEX_DISCOVERY_PORT);
+		_discovery_listener.start();
+	}
+
+	return _discovery_listener;
+}
+
+module.exports = { discovery_listener: discovery_listener };
